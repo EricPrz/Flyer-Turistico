3e80d754830458bc14b9381a09fa51b9e28aba28
ericprz <neryo19media@gmail.com>
2024-07-15 11:30:53 +0200

    Improved backend code and html

--- backendImages.js
+++ backendImages.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const app = express()
 
-const url = process.cwd()
-console.log(url)
+const url = process.cwd() + '/images/'
+const port = process.env.PORT || 3000
 
 app.get('/:url', function (req, res) {
     const dir = req.params.url
-    console.log(dir)
     res.setHeader("Access-Control-Allow-Origin", "*")
-    res.sendFile(url + "/images/" + dir)
+    res.sendFile(url + dir)
 })
 
-app.listen(3000)
+app.listen(port)
---
+++ prueba.py
@@ -0,0 +1,2 @@
+f=open("d:\FlyerTuristico\PapelPublicitario.html", 'r')
+print(f.read())
